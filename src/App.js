98fa7c13039f17989df38a5b96f1b0b71bac4383
kseniavelichko <kvelichko@ufl.edu>
2023-04-05 19:13:51 -0400

    something

--- src/App.js
+++ src/App.js
@@ -3,15 +3,11 @@ import './App.css';
 
 function App() {
   return (
-    <div class="hero min-h-screen bg-base-100">
-		<div class="hero-content text-center">
-		  <div class="max-w-md">
+    <div class="min-h-screen bg-base-100">
 			<h1 class="text-5xl font-bold">Bookify</h1>
 			<p class="py-6">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
 			<button class="btn btn-primary">Get Started</button>
 		  </div>
-		</div>
-	  </div>
   );
 }
 export default App;
