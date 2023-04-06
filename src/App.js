5dc0ecff161a3d265b5682a8ce30369f5e7b3062
kseniavelichko <kvelichko@ufl.edu>
2023-04-06 11:13:05 -0400

    rough design prototype for homepage

--- src/App.js
+++ src/App.js
@@ -2,55 +2,37 @@ import './App.css';
 
 function App() {
   return (
-    <div class="min-h-screen bg-base-100">
-		<div class="navbar bg-base-100">
+    <div class="min-h-screen bg-secondary">
+		<div class="navbar">
   			<div class="flex-1">
-    			<a class="btn btn-ghost normal-case text-xl">daisyUI</a>
+    			<a class="btn btn-ghost normal-case text-xl">Bookify</a>
   			</div>
   			<div class="flex-none">
     			<div class="dropdown dropdown-end">
-      				<label tabindex="0" class="btn btn-ghost btn-circle">
-        			<div class="indicator">
-          				<svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z" /></svg>
-          				<span class="badge badge-sm indicator-item">8</span>
-        			</div>
-      				</label>
-      				<div tabindex="0" class="mt-3 card card-compact dropdown-content w-52 bg-base-100 shadow">
-        				<div class="card-body">
-          					<span class="font-bold text-lg">8 Items</span>
-          					<span class="text-info">Subtotal: $999</span>
-          					<div class="card-actions">
-            					<button class="btn btn-primary btn-block">View cart</button>
-          					</div>
-        				</div>
-      				</div>
-    			</div>
-    			<div class="dropdown dropdown-end">
       				<label tabindex="0" class="btn btn-ghost btn-circle avatar">
-        			<div class="w-10 rounded-full">
-          				<img src="/images/stock/photo-1534528741775-53994a69daeb.jpg" />
-        			</div>
+        			<div class="w-10 rounded-full bg-primary"></div>
       				</label>
       				<ul tabindex="0" class="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
-        			<li>
-          			<a class="justify-between">
-            		Profile
-            		<span class="badge">New</span>
-          			</a>
-        			</li>
-        			<li><a>Settings</a></li>
-        			<li><a>Logout</a></li>
+        			<li><a class="justify-between">Sign Up</a></li>
+        			<li><a>Login</a></li>
       				</ul>
     			</div>
 			</div>
   		</div>
 
-		<div class="hero-content text-center">
-		  <div class="max-w-md">
-			<h1 class="text-5xl font-bold">Bookify</h1>
-			<p class="py-6">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
-			<button class="btn btn-primary">Get Started</button>
-		  </div>
+		<div class="hero-content ">
+		  <div class="mt-16 ml-36">
+			<h1 class="text-9xl font-bold text-primary">Bookify</h1>
+			<h2 class="text-3xl">The Comprehensive Vacation Planner</h2>
+			<div class=" mt-6 ml-8 max-w-xs">
+			<div class="form-control">
+          			<input type="text" placeholder="Dream vacation spot..." class="input w-full" />
+        		</div>
+        		<div class="form-control mt-4">
+          			<button class="btn btn-primary">Get Started</button>
+        		</div>
+				</div>
+			</div>
 		</div>
 	</div>
   );
