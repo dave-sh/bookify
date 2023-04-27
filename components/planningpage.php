2579ba2e8cb073cf030f55869d14716346419198
David H <davhuang123@gmail.com>
2023-04-27 14:35:02 -0400

    changed redirect from vacations to planning page.
    added planning page front-end to select vacations.

--- components/planningpage.php
+++ components/planningpage.php
@@ -34,7 +34,7 @@
             <div class="dropdown dropdown-end">
                 <label tabindex="0" class="btn btn-ghost btn-circle avatar">
                     <div class="w-10 rounded-full">
-                        <img src="../images/empty.png" />
+                        <img src="../images/empty.png" alt="pfp"/>
                     </div>
                 </label>
                 <ul tabindex="0" class="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
