950b81d9015d538fca6ba2f5b0e80ed67e495de2
David H <davhuang123@gmail.com>
2023-04-27 14:36:44 -0400

    changed redirect from vacations to planning page.
    added planning page front-end to select vacations.

--- components/planningpage.php
+++ components/planningpage.php
@@ -14,7 +14,7 @@
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.4/jquery.min.js"></script>
     <title>Bookify</title>
 </head>
-<script type="text/javascript" src="js/jquery.js"></script>
+<!--<script type="text/javascript" src="js/jquery.js"></script>-->
 <script type="text/javascript">
     $(document).ready(function(){
         $("#nav").load("components/navbar.html");
