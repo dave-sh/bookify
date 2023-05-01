f609eb71a8b9a87898c0d18e84697d9677abadad
kseniavelichko <kvelichko@ufl.edu>
2023-05-01 15:51:17 -0400

    fixed nav

--- index.php
+++ index.php
@@ -27,15 +27,11 @@ session_start();
         <a href="../../bookify/index.php" class="hover:cursor-pointer hover:underline text-white font-bold text-4xl">Bookify</a>
     </div>
     <div class="flex-none">
-        <?php if (isset($_SESSION['isLoggedIn']) && $_SESSION['isLoggedIn']): ?>
-<!-- Display links for logged-in users -->
-<form action="#" method="post" class="text-white inline">
-  <button class="hover:underline mt-4 text-white text-xl font-bold">My Profile</button> |&nbsp;
-</form>
-<form action="#" method="get" class="inline">
-  <button class=" hover:underline mt-4 text-white text-xl font-bold mr-2">Logout</button>
-</form>
-<?php else: ?>
+        <?php if (isset($_SESSION['isLoggedIn']) && $_SESSION['isLoggedIn']):
+            header("location: components/vacations.php");
+            exit;
+          ?>
+        <?php else: ?>
 <!-- Display links for non-logged-in users -->
 <form action="backend/signup.html" method="post" class="text-white inline">
   <button class="hover:underline mt-4 text-white text-xl font-bold">Sign Up</button> |&nbsp;
